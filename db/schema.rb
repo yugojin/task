3681e5ae8b4b42f3471f4bce718dfef95035e50b
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2021-02-09 06:15:15 +0000

    fix seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_02_09_060939) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "state"
+    t.text "task"
+    t.date "limit_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
